7ad868f7136eb8fc0a22c4ee5d46c6933c729db1
Espen Riskedal <espen@cutehacks.com>
2015-03-12 10:53:19 +0100

    adding simple Espresso 2.0 UI test

---
+++ app/src/androidTest/java/com/cutehacks/automatictesting/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.cutehacks.automatictesting;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testHelloWorld() {
+        onView(withText(getActivity().getString(R.string.hello_world))).check(matches(isDisplayed()));
+    }
+}
