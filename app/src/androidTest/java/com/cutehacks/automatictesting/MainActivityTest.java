906765a3e506d5d65987171986f9d838d64aec11
Espen Riskedal <espen@cutehacks.com>
2015-03-13 10:17:09 +0100

    Take a screenshot with Robotium

--- app/src/androidTest/java/com/cutehacks/automatictesting/MainActivityTest.java
+++ app/src/androidTest/java/com/cutehacks/automatictesting/MainActivityTest.java
@@ -2,12 +2,16 @@ package com.cutehacks.automatictesting;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.robotium.solo.Solo;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    Solo solo;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -16,10 +20,11 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        getActivity();
+        solo = new Solo(getInstrumentation(), getActivity());
     }
 
     public void testHelloWorld() {
+        solo.takeScreenshot("Before testing Hello World!");
         onView(withText(getActivity().getString(R.string.hello_world))).check(matches(isDisplayed()));
     }
 }
